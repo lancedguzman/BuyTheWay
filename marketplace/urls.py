4e0ef02fd8bb72b823b9eca03e41477cb40d5a56
Goodymind <aabuke9@gmail.com>
2026-02-27 02:28:27 +0800

    chore(shopping-cart): restored original urls in marketplace/urls.py

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -6,7 +6,7 @@ app_name = 'marketplace'
 urlpatterns = [
     path('sh-cart/', views.cart_view, name='shopping_cart'),
     path('marketplace/', views.marketplace_view, name='marketplace'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/<int:pk>/', views.product_detail, name='product_view'),
     path('product/add/', views.add_product, name='add_product'),
     path('product/<int:pk>/edit/', views.edit_product, name='edit_product'),
 ]
