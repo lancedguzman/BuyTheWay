1a44e489a37d941c161796606c2d6150b8ae429d
Goodymind <aabuke9@gmail.com>
2026-02-27 02:27:46 +0800

    chore(shopping-cart): remove tester code in apps/view

--- apps/views.py
+++ apps/views.py
@@ -1,15 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
-def home(request):
-    return render(request, 'base.html') #just for testing
-
-def cart_view(request):
-    
-    return render(request, 'shopping_cart.html') #just for testing 2
-
-def buyer_product_view(request):
-    
-    return render(request, 'buyer_productview.html') #just for testing 3
