3f8c9f8e39ef9e9b6913dca16f419fe9e5675d95
icepicklife <hanzdrive01@gmail.com>
2026-02-27 00:48:50 +0800

    feat(buyer_product-view): developed view for buyers on the product detail [iteration-1]

--- apps/views.py
+++ apps/views.py
@@ -9,3 +9,7 @@ def home(request):
 def cart_view(request):
     
     return render(request, 'shopping_cart.html') #just for testing 2
+
+def buyer_product_view(request):
+    
+    return render(request, 'buyer_productview.html') #just for testing 3
--- core/urls.py
+++ core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('sh-cart/', views.cart_view, name='shopping_cart'),
+    path('buyer-product-view/', views.buyer_product_view, name='buyer_product_view'),
 ]
