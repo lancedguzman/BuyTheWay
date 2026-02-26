02bd64e64faa978d15926ed1418199bd1eab03d2
Lance de Guzman <lance.deguzman@countable.ca>
2026-02-26 16:59:34 +0800

    fix: removed apps.urls from core.urls[iteration-1]

--- core/urls.py
+++ core/urls.py
@@ -15,16 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.urls import path, include
 from django.conf.urls.static import static
-from apps import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('marketplace/', views.marketplace_view, name='marketplace'),
-    path('sh-cart/', views.cart_view, name='shopping_cart'),
+    path('', include('marketplace.urls')),
     path('account_management/', include('account_management.urls')),
     path("account_management/", include("django.contrib.auth.urls")),
 ]
