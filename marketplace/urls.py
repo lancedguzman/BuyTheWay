64cbc537e8bfc63cefaaa12c39db4635f8f8d216
Lance de Guzman <lance.deguzman@countable.ca>
2026-03-04 16:19:46 +0800

    feat: added QR payment for buyers[iteration-2]

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('product/<int:pk>/', views.product_view, name='product_view'),
     path('product/add/', views.add_product, name='add_product'),
     path('product/<int:pk>/edit/', views.edit_product, name='edit_product'),
+    path('checkout/', views.checkout_view, name='checkout'),
     path('transaction-history', views.seller_view_order_history, name='transaction-history'),
     path('orders/', views.order_history, name='order_history'),
 ]
