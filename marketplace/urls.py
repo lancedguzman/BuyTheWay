deb7318e0f55ef319539dc60a214e9cb9f6e74ba
Lance de Guzman <lance.deguzman@countable.ca>
2026-03-05 09:30:09 +0800

    fix: changed marketplace.urls[iteration-2]

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'marketplace'
 
 urlpatterns = [
+    path('', views.marketplace_view, name='marketplace'),
     path('sh-cart/', views.cart_view, name='shopping_cart'),
-    path('marketplace/', views.marketplace_view, name='marketplace'),
     path('product/<int:pk>/', views.product_view, name='product_view'),
     path('product/add/', views.add_product, name='add_product'),
     path('product/<int:pk>/edit/', views.edit_product, name='edit_product'),
